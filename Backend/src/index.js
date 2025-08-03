f598da945e9979e55bfcfc3c9fa47dac41b748e5
sankarshan <ssankarshan636@gmail.com>
2025-08-03 15:29:52 +0530

    Implement backend server setup with Express and MongoDB connection

---
+++ Backend/src/index.js
@@ -0,0 +1,25 @@
+import { app } from "./app.js";
+import dotenv from 'dotenv'
+import connectDB from "./Database/index.js";
+
+dotenv.config({
+    path: './.env'
+});
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    console.log("Hello World");
+    res.send("Hello World from Backend!");
+});
+app.listen(PORT, () => {    
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`MongoDB connected successfully on ${process.env.PORT || 4000}`);
+    });
+}).catch((error) => {
+    console.log(`MongoDB connection error: ${error}`);    
+})
