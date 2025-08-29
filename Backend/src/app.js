8842326cef684b7db2c8369cdf80fa4b63c9ad56
sankarshan <ssankarshan636@gmail.com>
2025-08-29 15:51:43 +0530

    fix(cors): enhance CORS configuration to allow multiple origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,23 +4,40 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://linked-in-iota-snowy.vercel.app'
+]
+
 app.use(cors({
-    // origin: 'http://localhost:5173',
-    origin: 'https://linked-in-iota-snowy.vercel.app',
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl)
+        if (!origin) return callback(null, true)
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true)
+        } else {
+            return callback(new Error('Not allowed by CORS'))
+        }
+    },
     credentials: true
 }))
 
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     // origin: 'https://linked-in-iota-snowy.vercel.app',
+//     credentials: true
+// }))
+
 app.use(express.json({
-    limit:'16kb'
+    limit: '16kb'
 }))
 app.use(express.urlencoded({
     extended: true,
-    limit:'16kb'
+    limit: '16kb'
 }))
-
 app.use(express.static('public'))
-
 app.use(cookieParser())
+
 
 import authRoutes from './routes/auth.route.js'
 app.use('/api/auth', authRoutes)
